2cf37b7356f92358760323281e515eb21571d625
TAANISHKA <tanishkasahu2311@gmail.com>
2024-09-10 01:51:29 +0530

    changes in profile page -> profile image setup. You can delete and upload the profile images. Also made changes in the css of it.

--- client/src/pages/profile/index.jsx
+++ client/src/pages/profile/index.jsx
@@ -1,5 +1,5 @@
 import { useAppStore } from "@/store";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { IoArrowBack } from "react-icons/io5";
 import { Avatar, AvatarImage } from "@/components/ui/avatar"
@@ -8,16 +8,18 @@ import { FaPlus, FaTrash } from "react-icons/fa";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { toast } from "sonner"
-import { UPDATE_PROFILE_ROUTE } from "@/utils/constants";
+import { ADD_PROFILE_IMAGE_ROUTE, HOST, REMOVE_PROFILE_IMAGE_ROUTE, UPDATE_PROFILE_ROUTE } from "@/utils/constants";
+import { apiClient } from "@/lib/api-client";
 
 const Profile = () => {
     const navigate = useNavigate();
     const {userInfo, setUserInfo} = useAppStore();
-    const [firstName, setFirstName] = useState("")
-    const [lastName, setLastName] = useState("")
-    const [image, setImage] = useState(null)
-    const [hovered, setHovered] = useState(false)
-    const [selectedColor, SetselectedColor] = useState(0)
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [image, setImage] = useState(null);
+    const [hovered, setHovered] = useState(false);
+    const [selectedColor, setSelectedColor] = useState(0);
+    const fileInputRef = useRef(null);
 
 
     useEffect(() => {
@@ -26,19 +28,22 @@ const Profile = () => {
             setLastName(userInfo.lastName);
             setSelectedColor(userInfo.color);
         }
-    }, [userInfo])
+        if (userInfo.image) {
+            setImage(`${HOST}/${userInfo.image}`);
+        }
+    }, [userInfo]);
 
     const validateProfile = () => {
         if(!firstName){
-            toast.error("First Name is required")
-            return false
+            toast.error("First Name is required");
+            return false;
         }
         if(!lastName){
-            toast.error("Last Name is required")
-            return false
+            toast.error("Last Name is required");
+            return false;
         }
-        return true
-    }
+        return true;
+    };
 
     const saveChanges = async () => {
         if(validateProfile()){
@@ -50,7 +55,7 @@ const Profile = () => {
                 );
                 if (response.status === 200 && response.data){
                     setUserInfo({...response.data})
-                    toast.success("Profile Updated Successfully!!")
+                    toast.success("Profile Updated Successfully!!");
                     navigate("/chat")
                 }
             } catch (error) {
@@ -61,17 +66,52 @@ const Profile = () => {
 
     const handleNavigate = () => {
         if(userInfo.profileSetup){
-            navigate("/chat")
+            navigate("/chat");
         } else {
             toast.error("Please Setup Profile!");
         }
     };
 
+    const handleFileInputClick = () => {
+        fileInputRef.current.click();
+    };
+
+    const handleImageChange = async (event) => {
+        const file = event.target.files[0];
+        console.log({ file });
+        if (file) {
+            const formData = new FormData();
+            formData.append("profile-image", file);
+            const response = await apiClient.post(ADD_PROFILE_IMAGE_ROUTE, formData,
+                {withCredentials : true,
+         });
+         if ( response.status === 200 && response.data.image ) {
+            setUserInfo({ ...userInfo, image: response.data.image });
+            toast.success("Image updated successfully.");
+         } 
+        }
+    };
+
+    const handleDeleteImage = async () => {
+        try {
+            const response = await apiClient.delete(REMOVE_PROFILE_IMAGE_ROUTE, {
+                withCredentials: true,
+            });
+            if (response.status === 200) {
+                setUserInfo({ ...userInfo, image: null });
+                toast.success("Image removed successfully.");
+                setImage(null);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return (
     <div className="bg-[#1b1c24] h-[100vh] flex items-center justify-center flex-col gap-10">
-        <div className="flex flex-co gap-10 w-[80vw] md:w-max">
+        <div className="flex flex-col gap-10 w-[80vw] md:w-max">
             <div onClick={handleNavigate}>
-                <IoArrowBack className="text-4-xl lg:text-6xl text-white/90 cursor-pointer" />
+                <IoArrowBack className="text-4xl lg:text-6xl text-white/90 cursor-pointer" />
             </div>
             <div className="grid grid-cols-2">
                 <div 
@@ -93,19 +133,28 @@ const Profile = () => {
                                     : userInfo.email.split("").shift()
                                 }
                             </div>
-                        )
-
-                        }
+                        )}
                     </Avatar>
                     {
-                        hovered && (
-                            <div className="absolute inset-0 flex items-center justify-center bg-black/50 rounded-full cursor-pointer">
-                                {image ? <FaTrash className="text-white text-3xl cursor-pointer" /> : <FaPlus className="text-white text-3xl cursor-pointer" />}
+                        hovered && ( 
+                            <div className="absolute inset-0 flex items-center justify-center bg-black/50 ring-fuchsia-50 rounded-full "
+                            onClick={ image ? handleDeleteImage : handleFileInputClick }
+                            >
+                                {image ? (
+                                <FaTrash className="text-white text-3xl cursor-pointer" /> 
+                            ):( 
+                            <FaPlus className="text-white text-3xl cursor-pointer" />
+                        )}
                             </div>
-                        )
-                    }
-
-                    {/* { <input type="text" /> } */}
+                        )}
+                <input 
+                type="file" 
+                ref={fileInputRef} 
+                className = "hidden" 
+                onChange = {handleImageChange} 
+                name="profile-image" 
+                accept=".png, .jpg, .jpeg, .svg, .webp"
+                /> 
                 </div>
                 <div className="flex min-w-32 md:min-w-64 flex-col gap-5 text-white items-center justify-center ">
                     <div className="w-full">
