5aae1690d47224e51a60a1dec7457af3e1baba39
Raghav <raghavbhandari082@gmail.com>
2024-09-11 03:35:04 +0530

    Chat layout Setup, empty chat screen set up, contacts container setup, chat container setup and designed, Profile info component added, Logout functionality added.

--- client/src/pages/chat/components/empty-chat-container/index.jsx
+++ client/src/pages/chat/components/empty-chat-container/index.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
+
 import Lottie from 'react-lottie'
 
 function EmptyChatContainer() {
   return (
-    <div className='flex-1 md:bg-[#1c1d25] md:flex flex-col justify-center intems-center hidden duration-1000 transition-all'>
+    <div className='flex-1 md:bg-[#1c1d25] md:flex flex-col justify-center items-center hidden duration-1000 transition-all'>
         <Lottie
         isClickToPauseDisabled={true}
         height={200}
         width={200}
         options={animationDefaultOptions}
-
         />
+        <div className="text-opacity-80 text-white flex flex-col gap-5 items-center mt-10 lg:text-4xl text-3xl transition-all duration-300 text-center">
+          <h3 className="poppins-medium">
+            Hi <span className="purple-500">!</span> Welcome To <span className="purple-500">Vaarta!!</span>
+          </h3>
+        </div>
 
     </div>
   )
--- client/src/pages/chat/index.jsx
+++ client/src/pages/chat/index.jsx
@@ -3,6 +3,8 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
 import ContactsContainer from "./components/contacts-container";
+import EmptyChatContainer from "./components/empty-chat-container";
+import ChatContainer from "./components/chat-container";
 
 const Chat = () => {
         const { userInfo } = useAppStore();
@@ -17,8 +19,8 @@ const Chat = () => {
     return (
         <div className="flex h-[100vh] text-white overflow-hidden">
             <ContactsContainer/>
+            {/* <EmptyChatContainer/> */}
             <ChatContainer/>
-            <EmptyChatContainer/>
         </div>
     );
 };
