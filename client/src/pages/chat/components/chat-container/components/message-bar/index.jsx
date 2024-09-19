164ed979a1c2952d62291bb459145c6bf73582ed
TAANISHKA <tanishkasahu2311@gmail.com>
2024-09-20 02:27:35 +0530

    get user contacts module and send, receive, download and upload files and images done. get API's of all contacts and creating UI of channel modal.

--- client/src/pages/chat/components/chat-container/components/message-bar/index.jsx
+++ client/src/pages/chat/components/chat-container/components/message-bar/index.jsx
@@ -5,11 +5,20 @@ import { IoSend } from "react-icons/io5";
 import  EmojiPicker  from "emoji-picker-react";
 import { useAppStore } from "@/store";
 import { useSocket } from "@/context/SocketContext";
+import { apiClient } from "@/lib/api-client";
+import { UPLOAD_FILE_ROUTE } from "@/utils/constants";
 
 const MessageBar = () => {
     const emojiRef = useRef();
+    const fileInputRef = useRef();
     const socket = useSocket();
-    const { selectedChatType, selectedChatData, userInfo } = useAppStore();
+    const { 
+        selectedChatType, 
+        selectedChatData, 
+        userInfo,
+        setIsUploading,
+        setFileUploadProgress,
+     } = useAppStore();
     const [message, setMessage] = useState("");
     const [emojiPickerOpen, setEmojiPickerOpen] = useState(false);
 
@@ -40,6 +49,47 @@ const MessageBar = () => {
             });
         }
     };
+
+    const handleAttachmentClick = () => {
+        if (fileInputRef.current){
+            fileInputRef.current.click();
+        }
+    };
+
+    const handleAttachmentChange = async (event) => {
+        try {
+            const file = event.target.files[0];
+            if(file) {
+                const formData = new FormData();
+                formData.append("file", file);
+                setIsUploading(true);
+                const response = await apiClient.post(UPLOAD_FILE_ROUTE, formData, {
+                    withCredentials: true,
+                    onUploadProgress: (data) => {
+                        setFileUploadProgress(Math.round((100 * data.loaded) / data.total));
+                    },            
+                });
+
+
+                if(response.status === 200 && response.data) {
+                    setIsUploading(false);
+                    if(selectedChatType === "contact") {
+                        socket.emit("sendMessage",{
+                            sender: userInfo.id,
+                            content: undefined,
+                            recipient: selectedChatData._id,
+                            messageType: "file",
+                            fileUrl: response.data.filePath,
+                        });
+                    }
+                }
+            }
+            console.log({ file });
+        } catch (error) {
+            setIsUploading(false);
+            console.log({ error });
+        }
+    };
  
   return (
     <div className="h-[10vh] bg-[#1c1d25] flex justify-center items-center px-8 mb-5 gap-6">
@@ -53,11 +103,12 @@ const MessageBar = () => {
             />
             <button className="text-neutral-500 focus:border-none focus:outline-none 
             focus:text-white duration-300 transition-all"
-            // onClick={}
+            onClick={handleAttachmentClick}
             >
 
                 <GrAttachment className="text-2xl" />
             </button>
+            <input type="file" className="hidden" ref={fileInputRef} onChange={handleAttachmentChange} />
             <div className="relative">
                 <button 
                   className="text-neutral-500 focus:border-none focus:outline-none 
--- client/src/pages/chat/components/contacts-container/index.jsx
+++ client/src/pages/chat/components/contacts-container/index.jsx
@@ -1,8 +1,31 @@
+import { useEffect } from "react";
 import NewDm from "./components/new-dm";
 import ProfileInfo from "./components/profile-info";
+import { apiClient } from "@/lib/api-client";
+import { GET_DM_CONTACTS_ROUTES } from "@/utils/constants";
+import { useAppStore } from "@/store";
+import ContactList from "@/components/contact-list";
+import CreateChannel from "./components/create-channel";
 
 
-function ContactsContainer() {
+const ContactsContainer = () => {
+
+  const { setDirectMessagesContacts, directMessagesContacts } = 
+  useAppStore();
+
+  useEffect(() => {
+    const getContacts = async () => {
+      const response = await apiClient.get(GET_DM_CONTACTS_ROUTES, {
+        withCredentials: true,
+      });
+      if (response.data.contacts) {
+        setDirectMessagesContacts(response.data.contacts);
+      }
+    };
+
+    getContacts()
+  },[]);
+
   return (
     <div className='relative md:w-[35vw] lg:w-[30vw] xl:w-[20vw] bg-[#1b1c24] border-r-2 border-[#2f303b] w-full'>
       <div className="pt-3">
@@ -13,10 +36,14 @@ function ContactsContainer() {
           <Title text="Direct Messages" />
           <NewDm />
         </div>
+        <div className="max-h-[38vh] overflow-y-auto scrollbar-hidden">
+          <ContactList contacts={ directMessagesContacts }/>
+        </div>
       </div>
       <div className="my-5">
         <div className="flex items-center justify-between pr-10">
           <Title text="Groups" />
+          <CreateChannel />
         </div>
       </div>
       <ProfileInfo />
